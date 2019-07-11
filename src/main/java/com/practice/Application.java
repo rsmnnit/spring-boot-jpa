181f5eb42a7ccfdc10d122851e34b268f3d76a28
Radhe Shyam Lodhi <radheshyamlodhi64@gmail.com>
2019-07-12 00:18:59 +0530

    Kafka get last N and N days messages

--- src/main/java/com/practice/Application.java
+++ src/main/java/com/practice/Application.java
@@ -15,12 +15,12 @@ import java.util.Map;
 public class Application {
     public static void main(String args[]) {
         SpringApplication app = new SpringApplication(Application.class);
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("server.port", "8083");
-        properties.put("server.servlet.context-path", "/practice");
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        Map<String, Object> properties = new HashMap<>();
+//        properties.put("server.port", "8083");
+//        properties.put("server.servlet.context-path", "/practice");
+//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 
-        app.setDefaultProperties(properties);
+//        app.setDefaultProperties(properties);
         app.run(args);
     }
 }
