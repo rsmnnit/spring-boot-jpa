e230bb8c48d2f5b5f5a7176f55b9138cee94575e
Radhe Shyam Lodhi <radheshyamlodhi64@gmail.com>
2019-07-17 00:22:56 +0530

    Produce message using kafka producer api

--- src/main/java/com/practice/spring/service/PersonKafkaService.java
+++ src/main/java/com/practice/spring/service/PersonKafkaService.java
@@ -1,9 +1,16 @@
 package com.practice.spring.service;
 
 import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +31,31 @@ public class PersonKafkaService {
 
     private static final Logger logger = LoggerFactory.getLogger(PersonKafkaService.class);
 
+    public Producer createProducer(){
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        return producer;
+    }
+
     public void sendMessage(String message) {
         logger.info(String.format("$$ -> Producing message --> %s", message));
         this.kafkaTemplate.send(TOPIC, message + new Date().getTime());
+    }
+
+    public void produceMessage(String message) {
+        Producer<Long, String> producer = createProducer();
+        producer.send(new ProducerRecord<Long, String>(TOPIC,new Date().getTime(),message));
     }
 
     @KafkaListener(topics = "users", groupId = "group_id")
@@ -41,7 +70,7 @@ public class PersonKafkaService {
         props.put(ConsumerConfig.GROUP_ID_CONFIG,
                 "KafkaExampleConsumer");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class.getName());
+                LongDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class.getName());
 
